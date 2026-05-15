8dcc67925b4657087641d9b248fa8786db65d867
Miguel Cortez <mickeycortez2002@gmail.com>
2026-05-15 13:15:07 -0600

    feat: Configurando modelo de Chat

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chat extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'type'
+    ];
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
